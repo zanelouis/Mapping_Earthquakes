65f406e33912f9e66176b8cc09f274c373797289
zanelouis <71358697+zanelouis@users.noreply.github.com>
2021-01-11 10:40:16 -0800

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,35 +2,46 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
 let streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  id: 'mapbox/satellite-streets-v11',
+  id: 'mapbox/streets-v11',
   accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
 streetmap.addTo(map);
+ 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9425, -118.4081],
-  [37.6213, -122.3790],
-  [30.1944, -97.67],
-  [43.6767, -79.6306],
-  [40.6397, -73.7788]
-];
-
-// Create a polyline using the line coordinates and make the line black.
-L.polyline(line, {
-  color: "blue",
-  weight: '4',
-  opacity: '0.5',
-  dashArray: '10, 10'
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h3>Airport code: " + feature.properties.faa + "</h3> <hr> <h3>Airport Name: " + feature.properties.name + "</h3>");
+  }
 }).addTo(map);
 
 
