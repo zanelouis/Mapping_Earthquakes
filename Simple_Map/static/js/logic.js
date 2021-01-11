7952c8776a5532305105d94ded87edf6767f51b9
zanelouis <71358697+zanelouis@users.noreply.github.com>
2021-01-11 09:24:56 -0800

    git commit -m "added multi points on dark map"

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -5,18 +5,18 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
+var darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+  id: 'mapbox/dark-v10',
+	accessToken: API_KEY
 });
 
-function markerSize(population) {
-  return population / 40;
-}
+// Then we add our 'graymap' tile layer to the map.
+darkmap.addTo(map);
 
-// Get data from cities.js
-let cityData = [{
+// An array containing each city's location, state, and population.
+let cities = [{
   location: [40.7128, -74.0059],
   city: "New York City",
   state: "NY",
@@ -48,20 +48,15 @@ let cityData = [{
 }
 ];
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
-  L.marker(city.location)
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: "orange" 
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 .addTo(map);
 });
-
-L.circle([cityData.location], {
-  color: "blue",
-  fillColor: "blue",
-  fillOpacity: 0.75,
-  radius: cityData.population/200000
-}).addTo(myMap);
