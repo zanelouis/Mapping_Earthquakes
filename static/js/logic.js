fef3312bb348ec24c5c261ddac4927b0ddb3cae7
zanelouis <71358697+zanelouis@users.noreply.github.com>
2021-01-11 20:04:38 -0800

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,30 +1,30 @@
 // We create the tile layer that will be the background of our map.
-let DayNavigation = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  id: 'mapbox/light-v10',
+  id: 'mapbox/streets-v11',
   accessToken: API_KEY
 });
 
 // We create the tile layer that will be the background of our map.
-let NightNavigation = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  id: 'mapbox/dark-v10',
+  id: 'mapbox/satellite-streets-v11',
   accessToken: API_KEY
 });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  "Day Navigation": DayNavigation,
-  "Night Navigation": NightNavigation
+  “Streets”: streets,
+  “Satellite Streets”: satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44.0, -80.0],
-  zoom: 2,
-  layers: [DayNavigation]
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
